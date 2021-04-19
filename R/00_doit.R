c8c144738d2397a8aef910539b5e07e81014e725
najabahren <s164526@student.dtu.dk>
2021-04-19 09:39:25 +0000

    setup

---
+++ R/00_doit.R
@@ -0,0 +1,4 @@
+# Run all scripts ---------------------------------------------------------
+source(file = "R/01_load.R")
+source(file = "R/02_clean.R")
+source(file = "R/03_augment.R")
---
+++ R/03_augment.R
@@ -0,0 +1,10 @@
+# Clear workspace ---------------------------------------------------------
+rm(list = ls())
+
+
+# Load libraries ----------------------------------------------------------
+library("tidyverse")
+
+
+# Define functions --------------------------------------------------------
+source(file = "R/99_project_functions.R")
