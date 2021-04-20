c1c49da9c2fb2bb9ee7e5fc48b9656e27ae5e0bd
xxxsille <sille.hendriksen@hotmail.com>
2021-04-20 07:44:20 +0000

    test

--- R/01_load.R
+++ R/01_load.R
@@ -4,7 +4,12 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("usethis")
+library("devtools")
+install_github('ramhiser/datamicroarray')
+library("datamicroarray")
 
 
 # Define functions --------------------------------------------------------
-source(file = "R/99_project_functions.R")
+#source(file = "R/99_project_functions.R")
+data('alon', package = 'datamicroarray')
