6274be2d149d4563d53d40177e2a84ec15e9ef94
najabahren <s164526@student.dtu.dk>
2021-04-23 08:53:12 +0000

    Update of loading data

--- R/01_load.R
+++ R/01_load.R
@@ -6,10 +6,25 @@ rm(list = ls())
 library("tidyverse")
 library("usethis")
 library("devtools")
-install_github('ramhiser/datamicroarray')
-library("datamicroarray")
 
 
-# Define functions --------------------------------------------------------
-#source(file = "R/99_project_functions.R")
-data('alon', package = 'datamicroarray')
+# Load data ---------------------------------------------------------------
+load("data/_raw/alon.RData")
+
+
+#Data consist of 62 samples (rows) from 2000 genes (columns)
+# x = all measurements
+# y = classes, n = normal tissue (22), t = tumor tissue (40)
+
+# Write data --------------------------------------------------------------
+write_tsv(x = alon %>% 
+            pluck("x") %>% 
+            as_tibble,
+          file = "data/alon_x.tsv.gz")
+
+write_tsv(x = alon %>% 
+            pluck("y") %>% 
+            as_tibble,
+          file = "data/alon_y.tsv.gz")
+
+
