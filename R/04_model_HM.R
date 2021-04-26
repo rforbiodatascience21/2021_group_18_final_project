0d02ae69d7e07954ce5d7b55f08d0c48cad84b4e
xxxsille <sille.hendriksen@hotmail.com>
2021-04-26 08:30:54 +0000

    heatmap updated

--- R/04_model_HM.R
+++ R/04_model_HM.R
@@ -1,5 +1,6 @@
 # Wrangle data
-alon_clean_aug_long = alon_clean_aug %>%  pivot_longer(cols = !tissue, names_to = "Gene", values_to = "Expr_level") 
+alon_clean_aug_long = alon_clean_aug %>%  
+  pivot_longer(cols = !tissue, names_to = "Gene", values_to = "Expr_level") 
 
 # Nesting data
 alon_clean_aug_long_nested <- alon_clean_aug_long %>%
@@ -7,5 +8,24 @@ alon_clean_aug_long_nested <- alon_clean_aug_long %>%
   nest() %>%
   ungroup()
 
-ggplot(alon_clean_aug_long, mapping = aes(x =Gene, y=Expr_level, fill= tissue)) + 
+# Heatmap
+ggplot(alon_clean_aug_long, aes(x = tissue, y = Gene, fill = Expr_level)) +
+  geom_tile()
+
+# Heat map with groupimg and count of expression
+ggplot(alon_clean_aug_long %>% 
+         group_by(Gene, tissue) %>% 
+         count(Expr_level), 
+       mapping = aes(x = tissue, y = Gene, fill = Expr_level)) +
+  geom_tile()
+
+
+# Making random data to get fewer genes
+alon_clean_aug_long_random <- alon_clean_aug %>%
+  select(starts_with("X100") | contains("tissue")) %>% 
+  pivot_longer(cols = !tissue, names_to = "Gene", values_to = "Expr_level") 
+
+# Heatmap of the random genes 
+############################# SPG, ved ikke helt hvordan det bestemmer Expr_level...
+ggplot(alon_clean_aug_long_random %>% group_by(Gene, tissue) %>% count(Expr_level), aes(x = tissue, y = Gene, fill = Expr_level)) +
   geom_tile()
