3df893bc60d68fbcb910cfe64fc4b4744a44607f
najabahren <s164526@student.dtu.dk>
2021-04-26 08:07:53 +0000

    try

--- R/04_model_PCA.R
+++ R/04_model_PCA.R
@@ -2,8 +2,8 @@
 alon_clean_aug <- read_tsv(file = "data/alon_clean_aug.tsv.gz")
 
 # Wrangle data
-alon_clean_aug_long = alon_clean_aug %>%
-  pivot_longer(cols = -tissue, -c(tissue_discrete), "gene", values_to = "log2_expr_level") 
+alon_clean_aug_long = alon_clean_aug %>%  
+  pivot_longer(cols = -c(tissue, tissue_discrete), names_to = "Gene", values_to = "Expr_level")  
 
 # Nesting data
 alon_clean_aug_long_nested <- alon_clean_aug_long %>%
