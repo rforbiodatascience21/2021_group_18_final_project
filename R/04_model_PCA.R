8bc36e9b2305f2ed22c59927a12ceffc750a1a11
SofiaOtero <s164527@student.dtu.dk>
2021-04-26 08:05:52 +0000

    v

--- R/04_model_PCA.R
+++ R/04_model_PCA.R
@@ -2,11 +2,18 @@
 alon_clean_aug <- read_tsv(file = "data/alon_clean_aug.tsv.gz")
 
 # Wrangle data
-alon_clean_aug_long = alon_clean_aug %>%  pivot_longer(cols = !tissue, names_to = "Gene", values_to = "Expr_level") 
+alon_clean_aug_long = alon_clean_aug %>%
+  pivot_longer(cols = -tissue, -c(tissue_discrete), "gene", values_to = "log2_expr_level") 
 
 # Nesting data
 alon_clean_aug_long_nested <- alon_clean_aug_long %>%
-  group_by(Gene) %>%
+  group_by(gene) %>%
   nest() %>%
-  ungroup()
+  ungroup() %>% 
+  cols(-tissue_discrete)
+
+gravier_data_long_nested <- gravier_data_long_nested %>% 
+  mutate(mdl = map(data, ~glm(outcome ~ log2_expr_level,
+                              data =.,
+                              family = binomial(link = "logit"))))
 
