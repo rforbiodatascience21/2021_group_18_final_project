47fac14a680c24c481faef5c3328f607af33ac0c
najabahren <s164526@student.dtu.dk>
2021-04-26 10:01:28 +0000

    Load data

--- R/01_load.R
+++ R/01_load.R
@@ -24,9 +24,17 @@ data('alon', package = 'datamicroarray')
 
 
 # Load data ---------------------------------------------------------------
-read_csv("data/_raw/77_cancer_proteomes_CPTAC_itraq.csv")
-read_csv("data/_raw/clinical_data_breast_cancer.csv")
-read_csv("data/_raw/PAM50_proteins.csv")
+proteomes <- read_csv("data/_raw/77_cancer_proteomes_CPTAC_itraq.csv")
+clinical <- read_csv("data/_raw/clinical_data_breast_cancer.csv")
+proteins <- read_csv("data/_raw/PAM50_proteins.csv")
 
+# Write data --------------------------------------------------------------
+write_csv(x = proteomes, 
+          file = "data/proteomes.tsv.gz")
 
+write_csv(x = clinical, 
+          file = "data/clinical.tsv.gz")
+
+write_csv(x = proteins, 
+          file = "data/proteins.tsv.gz")
 
