8fbcde2eeef9f196563dc54a2eb6cb33517f1565
xxxsille <sille.hendriksen@hotmail.com>
2021-04-26 08:48:13 +0000

    updated with data

--- R/04_model_HM.R
+++ R/04_model_HM.R
@@ -1,3 +1,6 @@
+#Load data
+alon_clean_aug <- read_tsv(file = "data/alon_clean_aug.tsv.gz")
+
 # Wrangle data
 alon_clean_aug_long = alon_clean_aug %>%  
   pivot_longer(cols = -c(tissue, tissue_discrete), names_to = "Gene", values_to = "Expr_level")  
