b9ff5256b904aa692bc3e7e736d2fc7993d089c1
xxxsille <sille.hendriksen@hotmail.com>
2021-04-26 06:12:37 +0000

    test

--- R/02_clean.R
+++ R/02_clean.R
@@ -5,6 +5,17 @@ rm(list = ls())
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
 
+# Load data ---------------------------------------------------------------
+alon_x <- read_tsv(file = "data/alon_x.tsv.gz")
+alon_y <- read_tsv(file = "data/alon_y.tsv.gz")
+
+# Wrangle data ------------------------------------------------------------
+alon_clean <- bind_cols(alon_x, alon_y)
+#if size is different, the bind_cols can be dangerous
+
+# Write data --------------------------------------------------------------
+write_tsv(x = alon_clean,
+          path = "data/alon_clean.tsv.gz")
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
--- R/03_augment.R
+++ R/03_augment.R
@@ -5,6 +5,19 @@ rm(list = ls())
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
 
+# Load data ---------------------------------------------------------------
+alon_clean <- read_tsv(file = "data/alon_clean.tsv.gz")
+
+#Usikker på om det her skal gøre - men mutate til binary factor?
+# Wrangle data ------------------------------------------------------------
+alon_clean_aug <- alon_clean %>%
+  mutate(tissue = case_when(value == "n" ~ 0, 
+                             value == "t" ~ 1))%>%
+  select(tissue,everything(),-value)
+
+# Write data --------------------------------------------------------------
+write_tsv(x = alon_clean_aug,
+          path = "data/alon_clean_aug.tsv.gz")
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
