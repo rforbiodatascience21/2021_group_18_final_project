15653d221741eacc0962ae3c5afa8d42b1da2d45
xxxsille <sille.hendriksen@hotmail.com>
2021-04-29 13:28:44 +0000

    Get to know your data

---
+++ R/04_model_generel.R
@@ -0,0 +1,46 @@
+# Clear workspace ---------------------------------------------------------
+rm(list = ls())
+
+# Load data ---------------------------------------------------------------
+#alon_clean <- read_tsv(file = "data/alon_clean.tsv.gz")
+joined_data
+
+# Get to know your data
+nrow(joined_data) #80 observations
+ncol(joined_data) #31 variables
+
+# How many females and males in the data:
+joined_data %>% 
+  count(Gender)
+
+# Count the OS (overall survival) grouped by gender (1 = dead, 0 = survial)
+joined_data %>% 
+  group_by(Gender) %>%
+  count(`OS event`)
+
+
+
+# The age at initial pathologic diagnosis
+##### na.rm is to remove all NAs
+ggplot(joined_data,
+       mapping = aes(x =`Age at Initial Pathologic Diagnosis`, fill = Gender)) +
+  geom_bar(na.rm = T)
+
+# Scatterplot of age vs tumor filled by gender:
+ggplot(joined_data,
+       mapping = aes(x = `Age at Initial Pathologic Diagnosis`,
+                     y = Tumor, 
+                     color = Gender)) +
+  geom_point() +
+  labs()
+
+
+# Scatterplot of cancer stage vs tumor size filled by metastasis if positive or negative:
+ggplot(joined_data,
+       mapping = aes(x = `AJCC Stage`,
+                     y = Tumor, 
+                     color = `Metastasis-Coded`)) +
+  geom_point() +
+  labs()
+
+
