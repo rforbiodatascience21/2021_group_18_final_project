02e6f95e02e9dc6b647378d4b24c64152d1c82e2
najabahren <s164526@student.dtu.dk>
2021-05-03 08:23:16 +0000

    update

--- R/03_augment.R
+++ R/03_augment.R
@@ -11,23 +11,24 @@ clinical_clean <- read_csv(file = "data/clinical_clean.csv.gz")
 
 # Wrangle data ------------------------------------------------------------
 
-#Transpose data
+#Transpose data (get protein ID as columns)
 proteomes_clean_trans <- proteomes_clean_NA %>%
-  select(-Frac_NA)%>%
+  select(-Frac_NA) %>%
   pivot_longer(cols = -c("RefSeqProteinID"),
                names_to = "TCGA_ID",
                values_to = "value" ) %>% 
   pivot_wider(names_from = "RefSeqProteinID",
               values_from = "value") 
 
-#Join data
+#Join data to get one file (inner join to make sure ID's are represented in both)
 joined_data <- proteomes_clean_trans %>%
-  right_join(x = clinical_clean, 
-             y = ., 
+  inner_join(x = clinical_clean, 
+             y = proteomes_clean_trans, 
              by = "TCGA_ID")
 
 # Write data --------------------------------------------------------------
-
+write_csv(x = joined_data, 
+          file = "data/joined_data.csv.gz")
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
