ce5d17e3eaa2a47a8572779b088feaff60f8bad6
xxxsille <sille.hendriksen@hotmail.com>
2021-05-03 10:07:47 +0000

    updated

--- R/04_model_HM.R
+++ R/04_model_HM.R
@@ -6,18 +6,30 @@ joined_data <- read_csv(file = "data/joined_data.csv.gz")
 
 # Heat Map ---------------------------------------------------------------
 
-## HM with 100 random samples - this is not optimal... ##
+## HM with 100 random samples for RefSeqProtin ID - this is not optimal... ##
 ggplot(data = joined_data %>%
-         select(-(1:29)) %>%
-         pivot_longer(cols = -c("TCGA_ID"),
+         select(-(1:31)) %>%
+         pivot_longer(cols = -c(TCGA_ID),
                       names_to = "RefSeqProteinID",
-                      values_to = "value" ) %>%
+                      values_to = "value", 
+                      values_drop_na = T) %>%
          sample_n(100),
        mapping = aes(x = RefSeqProteinID, y = TCGA_ID, fill = value)) + 
   geom_tile() +
   scale_fill_gradient(low = "red", high = "blue") + 
   ylab("Patient ID") + 
   xlab("Protein ID")
+
+
+
+test = joined_data %>%
+  select(-(1:29)) %>%
+  pivot_longer(cols = -c("TCGA_ID"),
+               names_to = "RefSeqProteinID",
+               values_to = "value" ) %>%
+  group_by(RefSeqProteinID) %>%
+  nest() %>%
+  ungroup()
 
 
 ## Trying to make a HM from nested data ##
