3beb5d7995d5e878e22c699ba922b765f5d0003f
xxxsille <sille.hendriksen@hotmail.com>
2021-05-03 09:20:08 +0000

    updated

--- R/04_model_HM.R
+++ R/04_model_HM.R
@@ -5,12 +5,34 @@ rm(list = ls())
 joined_data <- read_csv(file = "data/joined_data.csv.gz")
 
 # Heat Map ---------------------------------------------------------------
-ggplot(data = , 
-       mapping = aes(x = sample, y = GeneName, fill = value)) + 
+ggplot(data = joined_data %>%
+         select(-(1:29)) %>%
+         pivot_longer(cols = -c("TCGA_ID"),
+                      names_to = "RefSeqProteinID",
+                      values_to = "value" ) %>%
+         sample_n(100),
+       mapping = aes(x = RefSeqProteinID, y = TCGA_ID, fill = value)) + 
   geom_tile() +
   scale_fill_gradient(low = "red", high = "blue") + 
-  ylab("GeneName") + 
-  xlab("Patients")
+  ylab("Patient ID") + 
+  xlab("Protein ID")
+
+
+
+ggplot(data = joined_data %>%
+         select(-(1:29)) %>%
+       mapping = aes(x = TCGA_ID, y = as.tibble(data,), fill = value)) + 
+  geom_tile() +
+  scale_fill_gradient(low = "red", high = "blue") + 
+  ylab("") + 
+  xlab("Patient ID")
+
+
+
+nested = proteomes_clean_NA %>%
+  group_by() %>%
+  nest() %>%
+  ungroup()
 
 
 ### Det her virker men er med forkert data #### 
