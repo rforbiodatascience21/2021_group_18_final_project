35dd4f852b51273ad2b747e50fa094a8228aedcf
xxxsille <sille.hendriksen@hotmail.com>
2021-05-03 08:59:46 +0000

    updated

--- R/04_model_HM.R
+++ R/04_model_HM.R
@@ -2,9 +2,26 @@
 rm(list = ls())
 
 # Load data ---------------------------------------------------------------
-#alon_clean <- read_tsv(file = "data/alon_clean.tsv.gz")
-joined_data
-cancer_data
+joined_data <- read_csv(file = "data/joined_data.csv.gz")
 
 # Heat Map ---------------------------------------------------------------
+ggplot(data = , 
+       mapping = aes(x = sample, y = GeneName, fill = value)) + 
+  geom_tile() +
+  scale_fill_gradient(low = "red", high = "blue") + 
+  ylab("GeneName") + 
+  xlab("Patients")
 
+
+### Det her virker men er med forkert data #### 
+test <- proteomes_clean_NA %>%
+  pivot_longer(cols = -c("RefSeqProteinID"),
+               names_to = "TCGA_ID",
+               values_to = "value" )
+
+ggplot(data = test, 
+       mapping = aes(x = RefSeqProteinID, y = TCGA_ID, fill = value)) + 
+  geom_tile() +
+  scale_fill_gradient(low = "red", high = "blue") + 
+  ylab("GeneName") + 
+  xlab("Patients")
