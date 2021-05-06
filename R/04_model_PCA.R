fe703558872338b505d2801143764b4aeb701e4e
SofiaOtero <s164527@student.dtu.dk>
2021-05-06 13:34:53 +0000

    lækkerier

--- R/04_model_PCA.R
+++ R/04_model_PCA.R
@@ -1,4 +1,11 @@
+# Clear workspace ---------------------------------------------------------
+rm(list = ls())
 
+# Load data ---------------------------------------------------------------
+joined_data <- read_csv(file = "data/joined_data.csv.gz")
+
+
+# PCA ---------------------------------------------------------------
 proteomes_data <- joined_data %>%
   select(Class,NP_009231, 
          NP_000537, 
@@ -14,21 +21,31 @@ pca <- proteomes_data %>%
   prcomp(center = TRUE, scale=TRUE)
 
 
-#Plot PCA matrix points
+#Percent of variance explained in PC1-PC8
+pca %>%
+  tidy(matrix = "eigenvalues")%>%
+  ggplot(mapping = aes(PC, percent)) +
+  geom_col(fill = "blue", alpha = 0.6) +
+  scale_x_continuous(breaks = 1:8) +
+  scale_y_continuous(
+    labels = scales::percent_format(),
+    expand = expansion(mult = c(0, 0.01))) +
+  theme_light() +
+  ylab("Percent of variance explained")
 
+
+#Plot PCA matrix points
 pca %>%
   augment(proteomes_data) %>%
   ggplot(mapping = aes(x = .fittedPC1,
                        y = .fittedPC2,
                        color = Class)) +
-  geom_point()
+  geom_point() +
+  xlab("PC1") +
+  ylab("PC2") +
+  theme_light()
 
 
-#Column wise
-pca %>%
-  tidy(matrix = "eigenvalues")%>%
-  ggplot(mapping = aes(PC, percent)) +
-  geom_col()
 
 
 
