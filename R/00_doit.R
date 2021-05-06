85117bc5253a02bdcb22b6cf051d5f4e3dcc5316
MieDB <s155941@dtu.dk>
2021-05-06 09:37:08 +0000

    comment in doit

--- R/00_doit.R
+++ R/00_doit.R
@@ -6,4 +6,3 @@ source(file = "R/03_augment.R")
 ###Fra Leons FAQ-dokument:
 #How do we run the entire project incl. the presentation?
 # - Make sure to include a programmatic call to knitr at the end of you doit-script
-
