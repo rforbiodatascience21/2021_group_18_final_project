dc562b9359cfbc88f74fc113c2b1339889c9a5b1
SofiaOtero <s164527@student.dtu.dk>
2021-05-06 14:11:14 +0000

    PCA update

--- R/04_model_PCA.R
+++ R/04_model_PCA.R
@@ -6,8 +6,10 @@ joined_data <- read_csv(file = "data/joined_data.csv.gz")
 
 
 # PCA ---------------------------------------------------------------
+
+### Class
 proteomes_data <- joined_data %>%
-  select(Class,NP_009231, 
+  select(Class, NP_009231, 
          NP_000537, 
          NP_009125, 
          NP_000305, 
@@ -22,7 +24,7 @@ pca <- proteomes_data %>%
 
 
 #Percent of variance explained in PC1-PC8
-pca %>%
+PCA_percent1 <- pca %>%
   tidy(matrix = "eigenvalues")%>%
   ggplot(mapping = aes(PC, percent)) +
   geom_col(fill = "blue", alpha = 0.6) +
@@ -31,11 +33,11 @@ pca %>%
     labels = scales::percent_format(),
     expand = expansion(mult = c(0, 0.01))) +
   theme_light() +
-  ylab("Percent of variance explained")
+  ylab("Percentage of variance explained")
 
 
 #Plot PCA matrix points
-pca %>%
+PCA_plot1 <- pca %>%
   augment(proteomes_data) %>%
   ggplot(mapping = aes(x = .fittedPC1,
                        y = .fittedPC2,
@@ -46,8 +48,79 @@ pca %>%
   theme_light()
 
 
+#Arrow style
+pca %>%
+  tidy(matrix = "rotation")
+
+# define arrow style for plotting
+arrow_style <- arrow(
+  angle = 20, ends = "first", type = "closed", length = grid::unit(8, "pt")
+)
+
+# plot rotation matrix
+pca %>%
+  tidy(matrix = "rotation") %>%
+  pivot_wider(names_from = "PC", names_prefix = "PC", values_from = "value") %>%
+  ggplot(aes(PC1, PC2)) +
+  geom_segment(xend = 0, yend = 0, arrow = arrow_style) +
+  geom_text(
+    aes(label = column),
+    hjust = 1, nudge_x = -0.02, 
+    color = "#904C2F"
+  ) +
+  xlim(-1.25, .5) + ylim(-.5, 1) +
+  coord_fixed() + # fix aspect ratio to 1:1
+  theme_minimal_grid(12)
 
 
 
+
+
+### Tumor
+proteomes_data2 <- joined_data %>%
+  select(Tumor, NP_009231, 
+         NP_000537, 
+         NP_009125, 
+         NP_000305, 
+         NP_004351, 
+         NP_000446,
+         NP_004439,
+         NP_001002295)
+
+pca2 <- proteomes_data %>%
+  select(where(is.numeric)) %>%
+  prcomp(center = TRUE, scale=TRUE)
+
+
+#Percent of variance explained in PC1-PC8
+PCA_percent2 <- pca2 %>%
+  tidy(matrix = "eigenvalues")%>%
+  ggplot(mapping = aes(PC, percent)) +
+  geom_col(fill = "blue", alpha = 0.6) +
+  scale_x_continuous(breaks = 1:8) +
+  scale_y_continuous(
+    labels = scales::percent_format(),
+    expand = expansion(mult = c(0, 0.01))) +
+  theme_light() +
+  ylab("Percentage of variance explained")
+
+#Plot PCA matrix points
+PCA_plot2 <- pca2 %>%
+  augment(proteomes_data2) %>%
+  ggplot(mapping = aes(x = .fittedPC1,
+                       y = .fittedPC2,
+                       color = Tumor)) +
+  geom_point() +
+  xlab("PC1") +
+  ylab("PC2") +
+  theme_light()
+
+
+
+# Write data --------------------------------------------------------------
+ggsave(filename = "results/PCA_percent1.png", plot = PCA_percent1, width = 16, height = 9, dpi = 72)
+ggsave(filename = "results/PCA_plot1.png", plot = PCA_plot1, width = 16, height = 9, dpi = 72)
+ggsave(filename = "results/PCA_percent2.png", plot = PCA_percent2, width = 16, height = 9, dpi = 72)
+ggsave(filename = "results/PCA_plot2.png", plot = PCA_plot2, width = 16, height = 9, dpi = 72)
 
 
