b60a6da3a38b50fe3f79849e3cd6fd4903bd562a
SofiaOtero <s164527@student.dtu.dk>
2021-05-09 09:19:30 +0000

    update

--- R/04_model_PCA.R
+++ R/04_model_PCA.R
@@ -25,7 +25,7 @@ pca <- proteomes_data %>%
 
 
 #Percent of variance explained in PC1-PC8
-PCA_percent1 <- pca %>%
+PCA_percent <- pca %>%
   tidy(matrix = "eigenvalues")%>%
   ggplot(mapping = aes(PC, percent)) +
   geom_col(fill = "blue", 
@@ -40,7 +40,7 @@ PCA_percent1 <- pca %>%
 
 
 #Plot PCA matrix points
-PCA_plot1 <- pca %>%
+PCA_plot <- pca %>%
   augment(proteomes_data) %>%
   ggplot(mapping = aes(x = .fittedPC1,
                        y = .fittedPC2,
@@ -52,45 +52,6 @@ PCA_plot1 <- pca %>%
   theme(legend.key = element_rect(fill = "white", 
                                   colour = "black"),
         legend.title = element_text(face = "bold"))
-
-
-
-
-#Arrow style clustering, her skal der tilføjes det fra kmeans, så vi har 5 clusters
-pca %>%
-  tidy(matrix = "rotation")
-
-# define arrow style for plotting
-arrow_style <- arrow(
-  angle = 20, ends = "first", 
-  type = "closed", 
-  length = grid::unit(8, "pt")
-)
-
-# plot rotation matrix
-pca %>%
-  tidy(matrix = "rotation") %>%
-  pivot_wider(names_from = "PC", 
-              names_prefix = "PC", 
-              values_from = "value") %>%
-  ggplot(aes(PC1, PC2)) +
-  geom_segment(xend = 0, yend = 0, 
-               arrow = arrow_style) +
-  geom_text(
-    aes(label = column),
-    hjust = 1, 
-    nudge_x = -0.02, 
-    color = "#904C2F"
-  ) +
-  xlim(-1.25, .5) + 
-  ylim(-.5, 1) +
-  coord_fixed() + # fix aspect ratio to 1:1
-  theme_minimal_grid(12)
-
-
-### K-means
-library(tidymodels)
-library(patchwork)
 
 
 
@@ -141,7 +102,7 @@ PCA_plot2 <- pca2 %>%
 #Defining k (number of center in k-means)
 #k is equal to the number of unique classes
 k <- joined_data %>% 
-  select (Class) %>% 
+  select(Class) %>% 
   unique() %>%
   pull() %>% 
   length()
@@ -160,8 +121,7 @@ proteomes_class <- joined_data %>%
 # PCA on cancergenes
 pca_class <- proteomes_class %>%
   select(where(is.numeric)) %>%
-  prcomp(center = TRUE, 
-         scale=TRUE)
+  prcomp(center = TRUE, scale=TRUE)
 
 # Augment pca with class
 pca_class_aug <- pca_class %>%
@@ -186,6 +146,8 @@ pca_fit_aug <- pca_class_aug_org %>%
 pca_org_aug <- pca_fit_aug %>%
   augment(pca_class_aug_org) %>% 
   rename(cluster_pca = .cluster)
+
+pca_kmeans(Class)
 
 #Defining three plots - one with pca, second with kmeans of original data
 # third with kmeans of pca data
@@ -217,8 +179,8 @@ pl3 <- pca_org_aug %>%
 
 
 # Write data --------------------------------------------------------------
-ggsave(filename = "results/PCA_percent1.png", plot = PCA_percent1, width = 16, height = 9, dpi = 72)
-ggsave(filename = "results/PCA_plot1.png", plot = PCA_plot1, width = 16, height = 9, dpi = 72)
+ggsave(filename = "results/PCA_percent.png", plot = PCA_percent1, width = 16, height = 9, dpi = 72)
+ggsave(filename = "results/PCA_plot.png", plot = PCA_plot1, width = 16, height = 9, dpi = 72)
 ggsave(filename = "results/PCA_percent2.png", plot = PCA_percent2, width = 16, height = 9, dpi = 72)
 ggsave(filename = "results/PCA_plot2.png", plot = PCA_plot2, width = 16, height = 9, dpi = 72)
 
