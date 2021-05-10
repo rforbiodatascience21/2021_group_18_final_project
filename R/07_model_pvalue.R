ccd9b83ad94337f442155c3594a60a98c8bb90bc
najabahren <s164526@student.dtu.dk>
2021-05-10 08:04:51 +0000

    Update

--- R/07_model_pvalue.R
+++ R/07_model_pvalue.R
@@ -9,58 +9,66 @@ library("tidyverse")
 joined_data <- read_csv(file = "data/joined_data.csv.gz")
 proteomes_clean_NA <- read_csv(file = "data/proteomes_clean_NA.csv.gz")
 
-cancer_genes <-
-  c("NP_009231", 
-  "NP_000537", 
-  "NP_009125", 
-  "NP_000305", 
-  "NP_004351", 
-  "NP_000446",
-  "NP_004439",
-  "NP_001002295")
-
-proteomes_clean_long <- proteomes_clean_NA %>%
-  select(-c(`263d3f-I`, 
-            `blcdb9-I`, 
-            `c4155b-C`)) %>%
-  select(-Frac_NA) %>%
-  pivot_longer(cols = -c("RefSeqProteinID"),
-                names_to = "TCGA_ID",
-                values_to = "log2_expression")
-  
-proteomes_clean_long <- proteomes_clean_long %>%
+# Sample cancer genes
+sample_genes_cancer <- proteomes_clean_NA %>%
   filter(RefSeqProteinID == "NP_009231" | RefSeqProteinID == "NP_000537"
          | RefSeqProteinID == "NP_009125" | RefSeqProteinID == "NP_000305"
          | RefSeqProteinID == "NP_004351" | RefSeqProteinID == "NP_000446"
          | RefSeqProteinID == "NP_004439" | RefSeqProteinID == "NP_001002295")
 
+#Sampling 92 random genes, which cannot be the cancer genes
+set.seed(429)
+sample_genes_random <- proteomes_clean_NA %>%
+  filter(RefSeqProteinID != "NP_009231" | RefSeqProteinID != "NP_000537"
+         | RefSeqProteinID != "NP_009125" | RefSeqProteinID != "NP_000305"
+         | RefSeqProteinID != "NP_004351" | RefSeqProteinID != "NP_000446"
+         | RefSeqProteinID != "NP_004439" | RefSeqProteinID != "NP_001002295") %>%
+  sample_n(92)
 
-proteomes_nested <- joined_data %>%
+# Bind together, in order to contain random and cancer genes
+sample_100_genes <- bind_rows(sample_genes_cancer, 
+                              sample_genes_random)
+
+# Make long data (excluding healthy persons)
+sample_100_genes_long <- sample_100_genes %>%
+  select(-c(`263d3f-I`, 
+            `blcdb9-I`, 
+            `c4155b-C`, 
+            Frac_NA)) %>%
+  pivot_longer(cols = -c("RefSeqProteinID"),
+               names_to = "TCGA_ID",
+               values_to = "log2_expression")
+
+# Join and nest data to get HER2 positive/negative factor included
+sample_100_genes_nested <- joined_data %>%
   select("TCGA_ID",
          "HER2_binary") %>%
-  right_join(y = proteomes_clean_long,
-            by = "TCGA_ID") %>% 
+  right_join(y = sample_100_genes_long,
+             by = "TCGA_ID") %>% 
   select(-TCGA_ID) %>%
   group_by(RefSeqProteinID) %>%
   nest() %>%
   ungroup()
 
-
-proteomes_func <- proteomes_nested %>%
+# Make logistic expressions function
+proteomes_func <- sample_100_genes_nested %>%
   mutate(mdl = map(data,
-                 ~glm(HER2_binary ~ log2_expression, 
-                      data = .,
-                      family = binomial(link = "logit"))),
-       tidying = map(mdl, conf.int = TRUE, tidy)) %>%
+                   ~glm(HER2_binary ~ log2_expression, 
+                        data = .,
+                        family = binomial(link = "logit"))),
+         tidying = map(mdl, conf.int = TRUE, tidy)) %>%
   unnest(tidying)
 
+# Excluding the intercept 
 proteomes_func <- proteomes_func %>%
   filter(term == "log2_expression")
 
+# Identify which genes are significant and non-significant
 proteomes_func_sig <- proteomes_func %>%
   mutate(identified_as = case_when(p.value > 0.05 ~"Non-significant",
                                    p.value < 0.05 ~ "Significant"))
 
+# Make negative log-10 of p-value for better visualization
 proteomes_func_sig <- proteomes_func_sig %>%
   mutate(neg_log10_p = -log10(p.value))
 
@@ -81,6 +89,7 @@ manhplot <- ggplot(proteomes_func_sig,
     panel.grid.minor.x = element_blank(),
     axis.text.x = element_text(angle = 45, size = 4, vjust = 0.5)
   )
+
 
 # Write data --------------------------------------------------------------
 ggsave(filename = "results/manhplot.png", plot = manhplot, width = 16, height = 9, dpi = 72)
