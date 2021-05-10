944c4013084afd2cc4f7ce17dfdf916120b77679
MieDB <s155941@dtu.dk>
2021-05-10 07:47:01 +0000

    doit update

--- R/00_doit.R
+++ R/00_doit.R
@@ -2,8 +2,10 @@
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
-source(file = "R/04_model_generel")
-source(file = "R/04_model_HM")
+source(file = "R/04_model_generel.R")
+source(file = "R/05_model_HM.R")
+source(file = "R/06_model_PCA.R")
+source(file = "R/07_model_pvalue.R")
 
 ###Fra Leons FAQ-dokument:
 #How do we run the entire project incl. the presentation?
